6fffd39ef4c525f5ffa48273a65fb48da15cc6cb
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-10-31 18:08:55 -0500

    added the prompt to start the node

--- server.js
+++ server.js
@@ -21,3 +21,31 @@ connect.connect((err) =>{
         return res.send("There was an error connecting to the database.");
     } console.log("You're Connected!");
 });
+
+
+// adding prompt 
+const begin = () => {
+    inquirer.createPromptModule({
+        name: 'startQuestions',
+        type: 'list', 
+        message: 'What would you like to do?',
+        choices:['View all Employees', 'Add Employees','Update Employees','View all Roles', 'Add Roles', 'View all Departments', 'Add Departnments'],
+    })
+    .then((answer) => {
+        if(answer.startQuestions === 'View all Employees') {
+            viewEmployees();
+        } else if(answer.startQuestions === 'Add Employees'){
+            addEmployees();
+        } else if(answer.startQuestions === 'Update Employees'){
+            updateEmployees();
+        }else if(answer.startQuestions === 'View all Roles'){
+            viewRoles();
+        }else if(answer.startQuestions === 'Add Roles'){
+            addRoles();
+        }else if(answer.startQuestions === 'View all Departments'){
+            viewDepartments();
+        }else if(answer.startQuestions === 'Add Departnments'){
+            addDepartments();
+        }
+    })
+}
