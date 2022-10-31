2ce99b5489025fb95769820e14b243b13a95d5ce
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-10-31 15:58:04 -0500

    added the tables for each category in schema db

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,27 @@
 DROP DATABASE IF EXISTS tracker_employee;
 CREATE DATABASE tracker_employee;
 USE tracker_employee;
+
+
+CREATE TABLE Department (
+    iddepartment INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE Role(
+    idrole INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2),
+    iddepartment INT,
+    FOREIGN KEY (iddepartment) REFERENCES Department (iddepartment)
+);
+
+CREATE TABLE Employee (
+    idemployee INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    idrole INT,
+    manager_id INT NULL,
+    FOREIGN KEY (idrole) REFERENCES (idrole),
+    FOREIGN KEY (manager_id) REFERENCES employee(idemployee)
+)
